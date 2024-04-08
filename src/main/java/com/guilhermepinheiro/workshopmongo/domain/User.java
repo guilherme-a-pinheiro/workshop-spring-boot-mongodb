ea54147ea55197f150542f35d27b77ed1298e28e
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2024-04-08 20:38:18 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/guilhermepinheiro/workshopmongo/domain/User.java
+++ src/main/java/com/guilhermepinheiro/workshopmongo/domain/User.java
@@ -1,9 +1,14 @@
 package com.guilhermepinheiro.workshopmongo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable{
+@Document(collection = "user")
+public class User implements Serializable {
+    @Id
     private String id;
     private String name;
     private String email;
---
+++ src/main/java/com/guilhermepinheiro/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.guilhermepinheiro.workshopmongo.services;
+
+import com.guilhermepinheiro.workshopmongo.domain.User;
+import com.guilhermepinheiro.workshopmongo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+}
