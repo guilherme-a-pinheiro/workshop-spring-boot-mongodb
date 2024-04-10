bc95e835f15a5f6f26638d5c75d9ffc7b527b308
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2024-04-09 23:31:19 -0300

    criando Post com User aninhado

---
+++ src/main/java/com/guilhermepinheiro/workshopmongo/repositories/PostRepository.java
@@ -0,0 +1,10 @@
+package com.guilhermepinheiro.workshopmongo.repositories;
+
+import com.guilhermepinheiro.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
