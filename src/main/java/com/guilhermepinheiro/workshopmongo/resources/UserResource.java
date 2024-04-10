6c4128741edb1dac9d85e1cae2818e704f0481f1
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2024-04-10 12:51:19 -0300

    endpoint retornando posts de um usuario

--- src/main/java/com/guilhermepinheiro/workshopmongo/resources/UserResource.java
+++ src/main/java/com/guilhermepinheiro/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.guilhermepinheiro.workshopmongo.resources;
 
+import com.guilhermepinheiro.workshopmongo.domain.Post;
 import com.guilhermepinheiro.workshopmongo.domain.User;
 import com.guilhermepinheiro.workshopmongo.dto.UserDTO;
 import com.guilhermepinheiro.workshopmongo.services.UserService;
@@ -55,5 +56,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user.getPosts());
     }
 }
