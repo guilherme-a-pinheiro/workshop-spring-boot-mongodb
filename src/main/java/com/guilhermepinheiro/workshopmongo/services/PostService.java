ac04f65d8308680b212c78e1418a466656267b19
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2024-04-10 13:05:39 -0300

    obtendo post por id

---
+++ src/main/java/com/guilhermepinheiro/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.guilhermepinheiro.workshopmongo.services;
+
+import com.guilhermepinheiro.workshopmongo.domain.Post;
+import com.guilhermepinheiro.workshopmongo.domain.User;
+import com.guilhermepinheiro.workshopmongo.repositories.PostRepository;
+import com.guilhermepinheiro.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+    public Post findById(String id){
+        Optional<Post> post = repository.findById(id);
+        return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+
+}
